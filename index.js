8f83625f13cb287c157c1740fb091dffff9aa442
jhonathannc <jhonathannc@live.com>
2020-09-11 13:53:56 -0300

    included method: format sentences

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // Formatador de string padrão abnt
+var commingstr, formattedStr
+
 exports.formatStr = (string) => {
-    words = string.toLowerCase().split(' ')
-    for (index = 0; index < words.length - 1; index++) {
-        words[index] += ' '
+    commingstr = string
+
+    wordsArray = string.toLowerCase()
+        .split(' ')
+
+    wordsArray.forEach((word, index) => {
+        switch (index) {
+            case 0:
+                wordsArray[index] = word.charAt(0).toUpperCase() + word.slice(1)
+                break
+        }
+        if (word.indexOf('.') > -1 && (wordsArray[index + 1] != null)) {
+            wordarr = wordsArray[index + 1]
+            wordsArray[index + 1] = wordarr.charAt(0).toUpperCase() + wordarr.slice(1)
+            // console.log(wordsArray[index + 1])
+        }
+    })
+    formattedStr = wordsArray.join(' ')
+    messages()
+    return {
+        formattedStr
     }
+}
+
+const messages = () => {
+    console.log('|-> Comming: ' + commingstr)
+    console.log('|---')
+    console.log('|-> Outing: ' + formattedStr)
+    console.log('|---')
+    console.log('|---')
+    console.log('|-> Check the author of lib in: http://github.com/jhonathannc')
+    console.log('|-> Wanna contribute? Check in: http://github.com/jhonathannc/stringformatter')
 }
